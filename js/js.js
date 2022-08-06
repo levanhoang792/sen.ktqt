8f3b784222298d5bb6636cf20c3d0fd3add49e05
HoangLV <levanhoang792@gmail.com>
2022-08-06 09:36:00 +0700

    a

--- js/js.js
+++ js/js.js
@@ -7,3 +7,13 @@ fetch(apiUrl).then(response => {
 }).catch(err => {
     // Do something for an error here
 });
+
+$(document).ready(function () {
+    const buttonsMenu = `
+        <a href="index.html" class="btn btn-bir">04/08/2022</a>
+        <a href="05082022.html" class="btn btn-bir">05/08/2022</a>
+        <a href="06082022.html" class="btn btn-bir">06/08/2022</a>
+    `;
+
+    $("div.buttons").html(buttonsMenu);
+});
