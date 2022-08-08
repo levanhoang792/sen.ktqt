4ee03b330e64b810550f72a9fe32a2725d58afb5
HoangLV <levanhoang792@gmail.com>
2022-08-08 09:17:18 +0700

    .

--- js/js.js
+++ js/js.js
@@ -14,6 +14,7 @@ $(document).ready(function () {
         <a href="index.html" class="btn btn-bir">04/08/2022</a>
         <a href="05082022.html" class="btn btn-bir">05/08/2022</a>
         <a href="06082022.html" class="btn btn-bir">06/08/2022</a>
+        <a href="08082022.html" class="btn btn-bir">08/08/2022</a>
     `;
 
     $("div.buttons").html(buttonsMenu);
